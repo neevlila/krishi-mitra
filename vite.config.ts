b5766e1a091e9905790276affeba6767deae0a82
Neev Lila <nneev223@gmail.com>
2026-08-15 13:08:05 +0530

    Optimize mobile SEO loading performance: Revert custom manualChunks to default and remove preloadCssPlugin to eliminate FOUC layout shifts

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// Custom Vite plugin to preload the built CSS files asynchronously
-function preloadCssPlugin() {
-  return {
-    name: "preload-css-plugin",
-    transformIndexHtml(html: string) {
-      return html.replace(
-        /<link rel="stylesheet" crossorigin href="([^"]+)">/g,
-        '<link rel="preload" href="$1" as="style" onload="this.onload=null;this.rel=\'stylesheet\'" crossorigin><noscript><link rel="stylesheet" href="$1" crossorigin></noscript>'
-      );
-    }
-  };
-}
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -24,23 +11,11 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    preloadCssPlugin(),
     mode === "development" && componentTagger()
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    }
   },
 }));
